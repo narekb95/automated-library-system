4e871a891a9a84a58690bf9216330381bc354924
Narek Bojikian <narekb95@Nareks-MacBook-Air.local>
2017-11-28 19:14:43 +0100

    working complex examples

--- main.cpp
+++ main.cpp
@@ -2,10 +2,14 @@
 #include<functional>
 #include"lib.h"
 using namespace std;
-void bookMyOwnBookBuyer(LibOrganizer* lib, Book book)
+void bookMyOwnBookBuyer(LibOrganizer** lib, Book book)
 {
     cout << "I am using own handler to buy a book";
-    lib->buyBook(book.getTitle(), book.getAuthor(), book.getISBN());
+    (*lib)->buyBook(book.getTitle(), book.getAuthor(), book.getISBN());
+}
+void testCallback(Book book)
+{
+    cout << "call back testing function to book " << book.toString() << endl;
 }
 int main(){
 	LibOrganizer lib1;
@@ -36,33 +40,68 @@ int main(){
 				lib1.buyBook("1985", "george...", "987654321");
                 continue;
 			}
+            else
+            {
+                throw e;
+            }
 		}
         lib1.returnBook("haik", "1985");
 	}
     cout <<"End of testcase 1..\n\n\n\n";
     
-    LibOrganizer *lib2;
-    auto binder = std::bind(bookMyOwnBookBuyer, lib2, placeholders::_1);
-    lib2 = new LibOrganizer(binder);
-    cout << lib2->buyBook("hamlet", "shakespere", "555555").toString() << endl;
-    cout << lib2->addUser("eva").toString() << endl;
+    
+    LibOrganizer lib2(testCallback);
+    lib2.addUser("beshara");
+    lib2.buyBook("learn english", "toefl", "333333");
     for(int i = 0; i < 55; i++)
     {
         try
         {
-            lib2->borrowBook("eva", "hamlet");
+            lib2.borrowBook("beshara", "learn english");
+        }
+        catch (LibOrganizer::LibExceptions e) {
+            if(e == LibOrganizer::LibExceptions::bookNotFound)
+            {
+                lib2.buyBook("learn english", "toefl", "333333");
+                continue;
+            }
+            else
+            {
+                throw e;
+            }
+        }
+        lib2.returnBook("beshara", "learn english");
+    }
+    cout <<"End of testcase 2..\n\n\n\n";
+    
+    
+    
+    LibOrganizer *lib3;
+    std::function<void(Book)> binderTest = std::bind(bookMyOwnBookBuyer, &(lib3), placeholders::_1);
+    //std::function<void(Book)> binderTest =testCallback;
+    lib3 = new LibOrganizer(binderTest);
+    cout << lib3->buyBook("hamlet", "shakespere", "555555").toString() << endl;
+    cout << lib3->addUser("eva").toString() << endl;
+    for(int i = 0; i < 55; i++)
+    {
+        try
+        {
+            lib3->borrowBook("eva", "hamlet");
         }
         catch(LibOrganizer::LibExceptions e)
         {
             if(e == LibOrganizer::LibExceptions::bookNotFound)
             {
-                //should never be called
                 cout <<"The book doesn't exist\n";
+                lib3->buyBook("hamlet", "shakespere", "555555").toString();
+                continue;
             }
+            else
+                throw e;
         }
-        lib2->returnBook("eva", "hamlet");
+        lib3->returnBook("eva", "hamlet");
     }
     cout << endl;
-    delete lib2;
+    delete lib3;
 	return 0;
 }
